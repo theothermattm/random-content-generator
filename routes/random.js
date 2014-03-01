dd60efc382d3705b49e2ce93792914b1f4d96f2a
Matt M <tomm@theothermattm.com>
2014-03-01 00:31:43 -0500

    cleaning up formatting and some jslint issues

--- routes/random.js
+++ routes/random.js
@@ -5,11 +5,11 @@ var Readable = require('stream').Readable;
 /**
  *  Main generate random function
  *  which generates random data based on chance.js
- *  through a chunked stream. 
+ *  through a chunked stream.
  */
 exports.generateRandomStream = function(req, res) {
-	var rs;
-	var chance;
+    var rs;
+    var chance;
     var sizeInChars;
     var sizeInKb = req.query.sizeInKb;
     var chunkSize = req.query.chunkSize || 100;
@@ -17,14 +17,15 @@ exports.generateRandomStream = function(req, res) {
     var dontCacheQueryString = req.query.dontCache;
     var dontCache = true;
     var numberOfImages;
+    var i;
 
-    if( !sizeInKb || sizeInKb < 1 || sizeInKb > config.max_random_size_kb ) {
-    	res.status('400').send('sizeInKb parameter must be between 1 and ' + config.max_random_size_kb);
-    	return;
+    if (!sizeInKb || sizeInKb < 1 || sizeInKb > config.max_random_size_kb) {
+        res.status('400').send('sizeInKb parameter must be between 1 and ' + config.max_random_size_kb);
+        return;
     }
 
-    if( dontCacheQueryString ) {
-    	dontCache = parseInt(dontCacheQueryString);
+    if (dontCacheQueryString) {
+        dontCache = parseInt(dontCacheQueryString);
     }
 
     numberOfImages = numberOfImagesQueryString ? parseInt(numberOfImagesQueryString) : 0;
@@ -32,43 +33,41 @@ exports.generateRandomStream = function(req, res) {
     /* this is just an approximation of size for now */
     sizeInChars = sizeInKb * 1024;
 
-    rs = Readable();
+    rs = new Readable();
     chance = new Chance();
     var charCounter = 0;
     rs._read = function() {
-    	if( charCounter === 0 ) {
-    		rs.push('<html><head><title>random content</title></head><body>');
-    	}
-    	charCounter += chunkSize;
-    	if( charCounter <= sizeInChars ){
-    		rs.push(chance.string({length: chunkSize}));
-    	} 
-    	else {
-    		if( numberOfImages ) {
+        if (charCounter === 0) {
+            rs.push('<html><head><title>random content</title></head><body>');
+        }
+        charCounter += chunkSize;
+        if (charCounter <= sizeInChars) {
+            rs.push(chance.string({
+                length: chunkSize
+            }));
+        } else {
+            if (numberOfImages) {
 
-    			for(var i=0; i < numberOfImages; i++ ) {
-    				// TODO put different images and
-    				// distribute them through payload
-    				rs.push('<img src="images/cat.jpg">');
-    			}
+                for (i = 0; i < numberOfImages; i++) {
+                    // TODO put different images and
+                    // distribute them through payload
+                    rs.push('<img src="images/cat.jpg">');
+                }
 
-    		}
-    		rs.push('</body></html>')
-    		rs.push(null);
-    	}
+            }
+            rs.push('</body></html>')
+            rs.push(null);
+        }
     }
 
-    if( dontCache ) {
-    	res.set({
-			'Cache-Control' : 'no-cache, no-store, must-revalidate',
-			'Pragma' : 'no-cache',
-			'Expires': '0'
-		});
+    if (dontCache) {
+        res.set({
+            'Cache-Control': 'no-cache, no-store, must-revalidate',
+            'Pragma': 'no-cache',
+            'Expires': '0'
+        });
     }
 
     rs.pipe(res);
 
 };
-
-
-
